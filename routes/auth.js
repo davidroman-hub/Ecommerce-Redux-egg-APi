9b5e3ff33df7fe6ad8d89a41c08726aac1707d57
David <jobroman83@gmail.com>
2020-10-14 16:48:59 +0200

    task 4 creating userSchema

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 // import controllers
-const {createOrUpdateUser} = require('../controllers/authControllers')
+const {createOrUpdateUser} = require('../controllers/authControllers');
 
-router.get('/create-or-update-user', createOrUpdateUser )
+router.get('/create-or-update-user', createOrUpdateUser );
 
 
 module.exports = router;
