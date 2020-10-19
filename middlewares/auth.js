790e7a72fd2f35f5ca2b7b74ead36f87c5254e76
David <jobroman83@gmail.com>
2020-10-19 13:00:45 +0200

    task 7 bring all the user info from firebase to the console

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,19 @@ const admin = require('../firebase/index');
 
 // we need to check if the token is valid from firebase
 
-exports.authCheck = (req, res, next) =>{
-    console.log(req.headers); // token
+exports.authCheck = async (req, res, next) =>{
+    //console.log(req.headers); // token
+    try {
+        const firebaseUser = await admin
+        .auth()
+        .verifyIdToken(req.headers.authtoken);
+        console.log('firebase user in authcheck', firebaseUser)
+        req.user = firebaseUser;
+        next()
+    } catch (error) {
+        res.status(401).json({
+            error:'Invalid or expired token'
+        })
+    }
     next();
 }
