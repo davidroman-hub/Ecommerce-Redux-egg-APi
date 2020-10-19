9291a2489e35e3353623c8b2dc2b0d53aa7a3ebb
David <jobroman83@gmail.com>
2020-10-19 14:04:47 +0200

    task 8 creating or updating user with firebase

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,6 +1,24 @@
+// to create the user we need to populate the infor from firebase in authmiddlewares
+// and bring the model ==>
 
-exports.createOrUpdateUser = (req,res) => {
-    res.json({
-        data:'you hit create-or-update node app'
-    })
-}
+const User = require('../models/user');
+
+exports.createOrUpdateUser = async  (req,res) => {
+    const {name, picture, email} = req.user; // <== from firebase authmidleware
+    // when we have thes info we need to create
+                                            //find by email, updated name and picture that means
+    const user = await User.findOneAndUpdate({email: email}, {name:name, picture:picture}, {new:true}) // if exist user from database updated with the new info!
+
+    if(user){
+        console.log('user updated', user)
+        res.json(user)
+    } else{
+        const newUser = await new User({
+            email,
+            name,
+            picture,
+        }).save();
+        console.log('user created', newUser)
+        res.json(newUser)
+    }
+};
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,13 +8,13 @@ exports.authCheck = async (req, res, next) =>{
         const firebaseUser = await admin
         .auth()
         .verifyIdToken(req.headers.authtoken);
-        console.log('firebase user in authcheck', firebaseUser)
+        //console.log('firebase user in authcheck', firebaseUser)
+        // now how we have the info.. we need to put this in our database ad create an user with the same info!
         req.user = firebaseUser;
         next()
     } catch (error) {
         res.status(401).json({
             error:'Invalid or expired token'
-        })
+        });
     }
-    next();
-}
+};
