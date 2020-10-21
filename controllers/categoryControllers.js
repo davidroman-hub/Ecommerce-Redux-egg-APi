cb811ecbcb60015cfc01eecf3508fbaecb99013a
David <jobroman83@gmail.com>
2020-10-21 15:43:11 +0200

    task 14 completing the CRUD category with read, list, remove and update

--- controllers/categoryControllers.js
+++ controllers/categoryControllers.js
@@ -15,18 +15,38 @@ exports.createCategory = async (req,res) => {
 }
 
 exports.readCategory = async (req,res) => {
-    //
+    let category = await Category.findOne({slug: req.params.slug}).exec();
+    res.json(category);
 }
 
 exports.listCategories = async (req,res) => {
-    //
+                                                    // from the last that i created
+    const listAllCategories = await Category.find({}).sort({createdAt: -1}).exec()
+    res.json( listAllCategories )
 }
 
 exports.updateCategory = async (req,res) => {
-    //
+    const { name } = req.body;
+    try {
+        const updated = await Category.findOneAndUpdate(
+            {slug:req.params.slug}, 
+            {name:name, slug: slugify(name)},
+            {new:true}
+            );
+            res.json(updated)
+    } catch (error) {
+        console.log(error)
+        res.status(400).send('Category updated failed')
+    }
 }
 
 exports.removeCategory = async (req,res) => {
-    //
+    try {
+        const deleted = await Category.findOneAndDelete({slug: req.params.slug});
+        res.json(deleted)
+    } catch (error) {
+        console.log(error)
+        res.status(400).send('Category deleted failed')
+    }
 }
 
--- routes/category.js
+++ routes/category.js
@@ -20,9 +20,9 @@ const { createCategory,
     } = require('../controllers/categoryControllers');
 
 router.post('/category', authCheck, adminCheck, createCategory);
-router.get('/categories', authCheck, adminCheck, listCategories);
-router.get('/category/:slug', authCheck, adminCheck, readCategory);
+router.get('/categories', listCategories);
+router.get('/category/:slug', readCategory);
 router.put('/category/:slug', authCheck, adminCheck, updateCategory);
-router.delete('/category:slug', authCheck, adminCheck, removeCategory);
+router.delete('/category/:slug', authCheck, adminCheck, removeCategory);
 
 module.exports = router;
