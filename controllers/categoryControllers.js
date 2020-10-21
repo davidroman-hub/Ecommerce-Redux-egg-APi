afc785daab0c186030f28a250ade7a7d45d0b6ec
David <jobroman83@gmail.com>
2020-10-21 15:01:02 +0200

    task 13 creating a new category success

--- controllers/categoryControllers.js
+++ controllers/categoryControllers.js
@@ -1,24 +1,32 @@
 
 const Category = require('../models/category');
+const slugify = require('slugify');
 
 
-exports.createCategory = (req,res) => {
+exports.createCategory = async (req,res) => {
+    try {
+        const {name} = req.body
+        const category = await new Category({name:name,slug: slugify(name).toLowerCase()}).save();
+        res.json(category)
+    } catch (error) {
+        console.log(error)
+        res.status(400).send('Create category failed')
+    }
+}
+
+exports.readCategory = async (req,res) => {
     //
 }
 
-exports.readCategory = (req,res) => {
+exports.listCategories = async (req,res) => {
     //
 }
 
-exports.listCategories = (req,res) => {
+exports.updateCategory = async (req,res) => {
     //
 }
 
-exports.updateCategory = (req,res) => {
-    //
-}
-
-exports.removeCategory = (req,res) => {
+exports.removeCategory = async (req,res) => {
     //
 }
 
